20ba3eb9aeff5c3ceb7da04583b7b31841dd0ee7
Mateus Kulik <mateuskulik30@gmail.com>
2022-12-01 18:59:55 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Form from './components/Form';
 const INITIAL_STATE = {
   cardName: '',
   cardDescription: '',
-  cardAttr1: '',
-  cardAttr2: '',
-  cardAttr3: '',
+  cardAttr1: 0,
+  cardAttr2: 0,
+  cardAttr3: 0,
   cardImage: '',
-  cardRare: '',
+  cardRare: 'normal',
   cardTrunfo: false,
   hasTrunfo: false,
-  isSaveButtonDisabled: false,
+  isSaveButtonDisabled: true,
 };
 
 class App extends React.Component {
@@ -20,11 +20,32 @@ class App extends React.Component {
     ...INITIAL_STATE,
   };
 
+  validationFields = () => {
+    const { cardName, cardDescription, cardAttr1, cardAttr2,
+      cardAttr3, cardImage } = this.state;
+    const noventa = 90;
+    const duzentos = 210;
+    const validationCardName = cardName.length > 0;
+    const validationCardDescription = cardDescription.length > 0;
+    const validationCardImage = cardImage.length > 0;
+    const validationCardAttr1 = +cardAttr1 <= noventa && +cardAttr1 >= 0;
+    const validationCardAttr2 = +cardAttr2 <= noventa && +cardAttr2 >= 0;
+    const validationCardAttr3 = +cardAttr3 <= noventa && +cardAttr3 >= 0;
+    const validationAtributes = +cardAttr1 + +cardAttr2
+     + +cardAttr3 <= duzentos;
+
+    this.setState({
+      isSaveButtonDisabled: !(validationCardName && validationCardDescription
+        && validationCardImage && validationCardAttr1 && validationCardAttr2
+        && validationCardAttr3 && validationAtributes),
+    });
+  };
+
   onInputChange = ({ target }) => {
     const { name, value } = target;
     this.setState(() => ({
       [name]: value,
-    }));
+    }), this.validationFields);
   };
 
   render() {
